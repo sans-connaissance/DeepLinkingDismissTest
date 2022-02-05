1eef19b71e555f693fb806196b9f7dd22f1987b9
sans-connaissance <davemalicke@gmail.com>
2022-02-05 14:01:43 -0500

    STARTING OVER

--- DeepLinkingDismissTest/ContentView.swift
+++ DeepLinkingDismissTest/ContentView.swift
@@ -9,8 +9,61 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            Text("Tab1")
+                .tabItem { Image(systemName: "1.square") }
+            Tab2RootView()
+                .tabItem { Image(systemName: "2.square") }
+        }
+    }
+}
+
+struct Tab2RootView: View {
+    @State var toRoot = false
+    var body: some View {
+        NavigationView {
+            Tab2NoteView(level: 0)
+                .id(toRoot)          // << reset to root !!
+        }
+        .environment(\.rewind, $toRoot)        // << inject here !!
+    }
+}
+
+struct Tab2NoteView: View {
+    @Environment(\.rewind) var rewind
+    let level: Int
+
+    @State private var showFullScreen = false
+    var body: some View {
+        VStack {
+            Text(level == 0 ? "ROOT" : "Level \(level)")
+            NavigationLink("Go Next", destination: Tab2NoteView(level: level + 1))
+            Divider()
+            Button("Full Screen") { showFullScreen.toggle() }
+                .fullScreenCover(isPresented: $showFullScreen,
+                                        onDismiss: { rewind.wrappedValue.toggle() }) {
+                    Tab2FullScreenView()
+                }
+        }
+    }
+}
+
+struct RewindKey: EnvironmentKey {
+    static let defaultValue: Binding<Bool> = .constant(false)
+}
+
+extension EnvironmentValues {
+    var rewind: Binding<Bool> {
+        get { self[RewindKey.self] }
+        set { self[RewindKey.self] = newValue }
+    }
+}
+
+struct Tab2FullScreenView: View {
+    @Environment(\.presentationMode) var mode
+
+    var body: some View {
+        Button("Close") { mode.wrappedValue.dismiss() }
     }
 }
 
