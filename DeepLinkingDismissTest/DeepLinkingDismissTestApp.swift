a6f23f9edb65e1ff003e5ac62bcd3fbbc94ae566
sans-connaissance <davemalicke@gmail.com>
2022-02-05 10:03:27 -0500

    Initial Commit

---
+++ DeepLinkingDismissTest/DeepLinkingDismissTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepLinkingDismissTestApp.swift
+//  DeepLinkingDismissTest
+//
+//  Created by David Malicke on 2/5/22.
+//
+
+import SwiftUI
+
+@main
+struct DeepLinkingDismissTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
